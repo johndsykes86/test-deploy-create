6fc1eaf6c2b394fde5dda06f02428feaa6b93546
Johnathon Sykes <hireme@mrjsykes.com>
2017-08-08 14:54:04 -0700

    initial commit

---
+++ src/components/VIP.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// const VIP = (props) => {
+//   return (
+//     <h1>THE VIP 🎊 🍾🍾🍾 🎉 </h1>
+//   )
+// }
+class VIP extends React.Component {
+
+  componentDidMount() {
+    // this is where an API call for protected content would be made.
+  }
+
+  render() {
+    return (
+      <h1>THE VIP 🎊 🍾🍾🍾 🎉 </h1>
+    )
+  }
+}
+
+export default VIP
